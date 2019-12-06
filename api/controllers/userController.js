09fb2ef22cf1dac6a8ff71694975909ce54286a9
Fayçal BORDJAH <fbordjah@linagora.com>
2019-12-06 12:34:19 +0100

    Improve user controller

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -66,3 +66,109 @@ exports.login = function (req, res) {
         }
     })
 }
+
+exports.get_user = function (req, res) {
+  User.findOne({_id:req.params.id})
+         .then(user => {
+              if (!user) {
+                  res.status(404);
+                  return res.json({
+                      status: "404",
+                      message: "Could not find user."
+                  });
+              } else {
+                res.status(200);
+                return res.json({
+                    status: "200",
+                    message: "User fetched successfully.",
+                    user: user
+                });
+              }
+         })
+         .catch(err => {
+              res.status(500);
+              return res.json({
+                  status: "500",
+                  message: "Something went wrong."
+              });
+         });
+}
+
+exports.update_user = function (req, res) {
+  User.findOne({_id:req.params.id})
+         .then(user => {
+            if (!user) {
+                res.status(404);
+                return res.json({
+                    status: "404",
+                    message: "User not found.",
+                    user: req.body
+                });
+            } else {
+              User.updateOne(user, req.body, function(err, result) {
+                  if (err) {
+                      res.status(400);
+                      return res.json({
+                          status: "400",
+                          message: "Could not update user.",
+                          user: req.body,
+                      });
+                   } else {
+                     res.status(200);
+                     return res.json({
+                         status: "200",
+                         message: "User updated.",
+                         userOld: user,
+                         userNew: req.body
+                     });
+                   }
+              });
+            }
+         })
+         .catch(err => {
+            res.status(500);
+            return res.json({
+                status: "500",
+                message: "Something went wrong."
+            });
+         });
+}
+
+exports.delete_user = function (req, res) {
+  User.findOne({_id:req.params.id})
+         .then(user => {
+            if (!user) {
+                res.status(404);
+                return res.json({
+                    status: "404",
+                    message: "User not found."
+                });
+            } else {
+              User.deleteOne({_id:req.params.id}, function(err, result){
+                  if (err) {
+                      res.status(400);
+                      return res.json({
+                          status: "400",
+                          message: "Could not delete user.",
+                          userId: req.params.id
+                      });
+                   } else {
+                     res.status(200);
+                     return res.json({
+                         status: "200",
+                         message: "User deleted.",
+                         userId: req.params.id
+                     });
+                   }
+              });
+            }
+         })
+         .catch(err => {
+            res.status(500);
+            return res.json({
+                status: "500",
+                message: "Something went wrong."
+            });
+         });
+}
+
