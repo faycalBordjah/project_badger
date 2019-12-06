0d43c796c929f9b352bd75557db6a2f90ff77ff0
AlexandreBalle <alexballe35@gmail.com>
2019-12-06 09:30:59 +0100

    Modify app.js file

--- app.js
+++ app.js
@@ -5,15 +5,14 @@ const app = express();
 const hostname = '0.0.0.0';
 const port = 8080;
 const User = require('./api/models/userModel');
-const Session = require('./api/models/sessionModel');
+const Session       = require('./api/models/sessionModel');
+const sessionRoutes = require('./api/routes/sessionRoutes');
+const userRoutes    = require('./api/routes/userRoutes');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://mongo/ipssi2019', { useUnifiedTopology: true, useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
-
-const sessionRoutes = require('./api/routes/sessionRoutes');
-const userRoutes = require('./api/routes/userRoutes');
 
 sessionRoutes(app);
 userRoutes(app);
